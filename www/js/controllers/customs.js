f7399fc6fb0291d18d70bde6fe8f2d7a1032ef2f
Aider Emirsuinov <aideremir@gmail.com>
2015-09-17 00:51:10 +0300

    Update customs.js
    API moved to cabinet.biocard.com

--- www/js/controllers/customs.js
+++ www/js/controllers/customs.js
@@ -6,7 +6,7 @@ module.controller('customsController', function ($scope, $http) {
     ons.ready(function () {
 
         modal.show();
-        $http.get('http://biocard.com/api/customs?courierLogin=' + biocard.login + '&courierPassword=' + biocard.password).
+        $http.get('http://cabinet.biocard.com/api/customs?courierLogin=' + biocard.login + '&courierPassword=' + biocard.password).
             success(function (data, status) {
 
                 //console.log(data);
